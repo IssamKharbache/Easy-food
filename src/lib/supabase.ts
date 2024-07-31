de3264dc5af3618553fc9894199f142286eb9bd8
lefline <issamkharbache2@gmail.com>
2024-07-31 09:37:08 +0100

    add and configure supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://ksezlarwqdunffuxuaav.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzZXpsYXJ3cWR1bmZmdXh1YWF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0MTQ2NjEsImV4cCI6MjAzNzk5MDY2MX0.DGdKq2VlWZFeQJFn81VY46dW1CQ_f6YCB9_C3fPX12s";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
